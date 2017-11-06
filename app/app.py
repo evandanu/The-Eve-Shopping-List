168c24da0465b7c71467aa4352f08c7a0aca0b80
Everlyne <Everlyne>
2017-11-06 11:17:05 +0300

    app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template
+import os
+from flask import Flask,render_template, session, url_for,request,redirect
+from app import app, views
 
-app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return render_template('index.html')
+app= Flask(__name__, static_folder='design/UI')
 
- 
 if __name__ == "__main__":
     app.run()
